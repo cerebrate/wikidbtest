7f27d7b2c782c4d01f94b8eb1a5f3b105850e8f8
Alistair Young <avatar@arkane-systems.net>
2013-09-29 17:31:35 -0500

    Framework.

---
+++ WikiDatabase.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+#region header
+
+// ModernWiki - WikiDatabase.cs
+// 
+// Alistair J. R. Young
+// Arkane Systems
+// 
+// Copyright Arkane Systems 2012-2013.  All rights reserved.
+// 
+// Created: 2013-09-29 1:27 PM
+
+#endregion
+
+using System;
+using Microsoft.Isam.Esent;
+using Microsoft.Isam.Esent.Interop;
+using Microsoft.Isam.Esent.Interop.Windows8;
+
+namespace ArkaneSystems.ModernWiki.Database
+{
+    public sealed class WikiDbTest : IDisposable
+    {
+        private bool disposed;
+
+        /// <summary>
+        ///     Non-reentrancy lock for all WikiDatabase public methods.
+        /// </summary>
+        private object nonReentrancyLock = new object ();
+
+        #region Singleton
+
+        /// <summary>
+        ///     Lazy-initialization field for the singleton instance.
+        /// </summary>
+        private static readonly Lazy<WikiDatabase> Lazy = new Lazy<WikiDatabase> (() => new WikiDatabase ());
+
+        /// <summary>
+        ///     The singleton instance of the WikiDatabase.
+        /// </summary>
+        public static WikiDatabase Instance
+        {
+            get { return WikiDatabase.Lazy.Value; }
+        }
+
+        #endregion
+
+        /// <summary>
+        ///     Construct the WikiDatabase instance; create the database file, and make any internal
+        ///     structures that aren't there already.
+        /// </summary>
+        private WikiDatabase ()
+        { }
+
+        #region IDisposable Members
+
+        public void Dispose ()
+        {
+            if (!this.disposed)
+            {
+                // Run the cleanup routines.
+
+                // Suppress finalization.
+                GC.SuppressFinalize (this);
+
+                this.disposed = true;
+            }
+        }
+
+        #endregion
+
+        #region Destructor
+
+        ~WikiDatabase ()
+        {
+            // Dispose us if we have not been disposed.
+            if (!this.disposed)
+                this.Dispose ();
+        }
+
+        #endregion
+    }
+}
